9469df084a52506d2554979a3b8f7d018aa2e320
Jurriaan van der Laan <jurriaan.van.der.laan@asml.com>
2019-01-03 17:10:10 +0100

    elzar instructions

--- env_win.sh
+++ env_win.sh
@@ -6,3 +6,5 @@ export PATH=/c/Users/julaan/j4jas/j4jas/perl-win/c/bin:$PATH
 set PATH=%PATH%;C:\Users\julaan\j4jas\j4jas\perl-win\perl\bin
 set PATH=%PATH%;C:\Users\julaan\j4jas\j4jas\perl-win\perl\site\bin
 set PATH=%PATH%;C:\Users\julaan\j4jas\j4jas\perl-win\c\bin
+
+C:\Users\julaan\j4jas\j4jas\mojo_app>elzar script/mojo_app
